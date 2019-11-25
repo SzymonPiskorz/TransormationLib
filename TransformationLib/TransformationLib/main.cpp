2cc67d26e1e51732e6e1c84e3e451a9953077a0b
(Student) - Szymon Piskorz <c00242808@ITCARLOW.IE>
2019-11-25 11:01:09 +0000

    Changed vector3 and matrix3

---
+++ TransformationLib/TransformationLib/main.cpp
@@ -0,0 +1,14 @@
+/// <summary>
+/// @Author Szymon Piskorz
+/// @Date 25/11/2019
+/// @Student No: c00242808
+/// </summary>
+
+#include <iostream>
+
+int main()
+{
+
+	system("Pause");
+	return 0;
+}
